053072995fc65a2f3fc69a7adadace307b293876
Guess <zylinux@users.noreply.github.com>
2023-04-18 20:01:54 -0400

    update app.c
    add hello print

--- app.c
+++ app.c
@@ -8,3 +8,11 @@
 //ddd
 //min1
 //min2
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  printf ("Hello test_app (Yocto)!\n");
+	printf ("I am test_app_hello project\n");
+	return 0;
+} /* ----- End of main() ----- */
