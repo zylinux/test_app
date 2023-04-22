50c6345651146d2aafeca2eae648aa9164d078b5
min zhang <zhangmin1984121@gmail.com>
2023-04-21 23:04:03 -0400

    learn devtool upgrade command with tag v1.6

--- app.c
+++ app.c
@@ -19,5 +19,6 @@ int main (int argc, char **argv)
 	printf("Learn devtool upgrade command and tag to v1.3\n");
 	printf("Learn devtool upgrade command and tag to v1.4\n");
 	printf("Learn devtool upgrade command and tag to v1.5\n");
+	printf("Learn devtool upgrade command and tag to v1.6\n");
 	return 0;
 } /* ----- End of main() ----- */
